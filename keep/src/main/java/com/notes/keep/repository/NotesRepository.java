e52984249e8c9ac0a9480381d5a803aec597d376
Sushant Raj <sushantraj215@gmail.com>
2023-10-22 01:30:54 +0530

    added dev and prod config

--- keep/src/main/java/com/notes/keep/repository/NotesRepository.java
+++ keep/src/main/java/com/notes/keep/repository/NotesRepository.java
@@ -3,17 +3,18 @@ package com.notes.keep.repository;
 import com.notes.keep.model.Notes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
-@Repository
+@EnableJpaRepositories
 public interface NotesRepository extends JpaRepository<Notes, UUID> {
     Notes findByNoteId(UUID id);
 
     @Query("SELECT n FROM Notes n WHERE n.user.userId = :userId")
-    List<Notes> findAllNotesByuserId(@Param("userId") UUID userId);
+    List<Notes> findAllNotesByUserId(@Param("userId") UUID userId);
 }
 
