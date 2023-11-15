5ca0bffa2fbd1666ec55599305544e7575246025
Sushant Raj <66193982+Sushant0999@users.noreply.github.com>
2023-11-15 13:05:44 +0530

    Update and rename index.sda to index.js

---
+++ google_keep_clone/api/index.js
@@ -0,0 +1,47 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n/i18n';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#71797E',
+      navbar: '#F4B400',
+      backgroundColor: '#FFFFFF',
+      textColor: '#FFFFFF',
+      navMenuIconColor: '#000000',
+      modalBg: '#FFFFFF',
+    },
+    secondary: {
+      main: '#202124',
+      navbar: '#202124',
+      backgroundColor: '#202124',
+      textColor: '#FFFFFF',
+      navMenuIconColor: '#FFFFFF',
+      modalBg: '#D5D5D5',
+    }
+  },
+});
+
+const root = document.getElementById('root');
+
+const renderApp = () => {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <I18nextProvider i18n={i18n} />
+      <ThemeProvider theme={theme}>
+        <App />
+        <ToastContainer position='top-right' />
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+};
+
+renderApp();
