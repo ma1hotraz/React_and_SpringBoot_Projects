85006bed40aac11bb93ec9f0a599179a2c7376b6
Sushant Raj <66193982+Sushant0999@users.noreply.github.com>
2023-11-19 06:07:54 +0530

    Update webpack.config.js

--- google_keep_clone/webpack.config.js
+++ google_keep_clone/webpack.config.js
@@ -1,20 +1,22 @@
-// const HtmlWebPlugin = require('html-webpack-plugin');
-// const path = require('path');
-// module.exports = {
-//     context: __dirname,
-//     entry: './api/index.js',
-//     output: {
-//         path: path.resolve(__dirname, 'dist'),
-//         filename: 'index.js',
-//         publicPath: '/'
-//     },
-//     devServer: {
-//         historyApiFallback: true,
-//     },
-//     plugins: [
-//         new HtmlWebPackPlugin({
-//             template: path.resolve(__dirname, '/public/index.html'),
-//             filename: 'index.html'
-//         })
-//     ]
-// };
+const HtmlWebPlugin = require('html-webpack-plugin');
+const path = require('path');
+module.exports = {
+    context: __dirname,
+    entry: './api/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+        publicPath: '/'
+    },
+    devServer: {
+        // historyApiFallback: true,
+          disableHostCheck: true,
+
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: path.resolve(__dirname, '/public/index.html'),
+            filename: 'index.html'
+        })
+    ]
+};
