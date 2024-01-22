93d2085db90940e92f008dfdca1de4fe9aa6df5c
sushant0999 <sushantraj215@gmail.com>
2024-01-22 15:06:12 +0530

    added entrypoint

---
+++ keep/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Sleep to allow time for MySQL to start
+sleep 10
+
+# Additional MySQL commands if needed
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER 'user'@'%' IDENTIFIED BY 'user';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE notes_db;"
+
+
+# Keep the container running
+exec "$@"
