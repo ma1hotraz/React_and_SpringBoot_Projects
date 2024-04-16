2d26a74cefb4a91223dfeff0ed97afff19d82638
Shashank Malhotra <shashank.malhotra@netsmartz.net>
2024-04-16 12:30:57 +0530

    Added a custom component "MaterialUISwitch" and implemented it in place of react-toggle-slider to fix the issue

--- google_keep_clone/src/component/DrawerAppBar.jsx
+++ google_keep_clone/src/component/DrawerAppBar.jsx
@@ -1,35 +1,32 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import { ToggleSlider } from 'react-toggle-slider';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
-import ProfileModal from './ProfileModal';
-import TranslateButton from './TranslateButton';
-import TrashModal from '../component/TrashModal';
-import ArchiveModal from '../component/ArchiveModal';
-import Notification from './Notification';
-import SortingList from './SortingList';
-
-
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import ProfileModal from "./ProfileModal";
+import TranslateButton from "./TranslateButton";
+import TrashModal from "../component/TrashModal";
+import ArchiveModal from "../component/ArchiveModal";
+import Notification from "./Notification";
+import SortingList from "./SortingList";
+import MaterialUISwitch from "./MaterialUISwitch";
 
 const drawerWidth = 240;
 
 function DrawerAppBar(props) {
-
   const { window } = props;
 
   const [mobileOpen, setMobileOpen] = useState(false);
@@ -39,22 +36,40 @@ function DrawerAppBar(props) {
     <ArchiveModal modalBg={props.modalBg} />,
     <TrashModal modalBg={props.modalBg} />,
     <TranslateButton />,
-    <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-      <FontAwesomeIcon key="sun" style={{ marginRight: '20px', marginLeft: '10px' }} icon={faSun} size='2x' />
-      <ToggleSlider key="slider" onToggle={props.toggleMode} active={!props.active} />
-      <FontAwesomeIcon key="moon" style={{ marginLeft: '20px', marginRight: '10px' }} icon={faMoon} size='2x' />
+    <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+      <FontAwesomeIcon
+        key="sun"
+        style={{ marginRight: "20px", marginLeft: "10px" }}
+        icon={faSun}
+        size="2x"
+      />
+      <MaterialUISwitch onChange={props.toggleMode} checked={!props.active} />
+
+      <FontAwesomeIcon
+        key="moon"
+        style={{ marginLeft: "20px", marginRight: "10px" }}
+        icon={faMoon}
+        size="2x"
+      />
     </Box>,
     <Notification />,
-    <ProfileModal />
+    <ProfileModal />,
   ];
 
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
   };
 
-
   const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center', color: `${props.textColor}`, backgroundColor: `${props.themeColor}`, minHeight: '100vh'}}>
+    <Box
+      onClick={handleDrawerToggle}
+      sx={{
+        textAlign: "center",
+        color: `${props.textColor}`,
+        backgroundColor: `${props.themeColor}`,
+        minHeight: "100vh",
+      }}
+    >
       <Typography variant="h6" sx={{ my: 2 }}>
         {props.name}
       </Typography>
@@ -62,7 +77,7 @@ function DrawerAppBar(props) {
       <List>
         {navItems.map((item) => (
           <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }}>
+            <ListItemButton sx={{ textAlign: "center" }}>
               <ListItemText primary={item} />
             </ListItemButton>
           </ListItem>
@@ -71,32 +86,41 @@ function DrawerAppBar(props) {
     </Box>
   );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar component="nav" >
+      <AppBar component="nav">
         <Toolbar sx={{ backgroundColor: `${props.navbar}` }} in Toolbar>
           <IconButton
             color="transparent"
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
             <MenuIcon sx={{ color: `${[props.navMenuIconColor]}` }} />
           </IconButton>
           <Typography
             variant="h6"
             component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block', color: 'white', fontFamily: 'Poppins, sans-serif' } }}
+            sx={{
+              flexGrow: 1,
+              display: {
+                xs: "none",
+                sm: "block",
+                color: "white",
+                fontFamily: "Poppins, sans-serif",
+              },
+            }}
           >
             {props.name}
           </Typography>
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
             {navItems.map((item) => (
-              <Button key={item} sx={{ color: '#fff' }}>
+              <Button key={item} sx={{ color: "#fff" }}>
                 {item}
               </Button>
             ))}
@@ -113,8 +137,11 @@ function DrawerAppBar(props) {
             keepMounted: true,
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
         >
           {drawer}
@@ -127,12 +154,12 @@ function DrawerAppBar(props) {
 DrawerAppBar.propTypes = {
   window: PropTypes.func,
   name: PropTypes.string,
-  nav: PropTypes.string
+  nav: PropTypes.string,
 };
 
 DrawerAppBar.defaultProps = {
-  nav: 'yellow',
-  name: 'null'
-}
+  nav: "yellow",
+  name: "null",
+};
 
 export default DrawerAppBar;
